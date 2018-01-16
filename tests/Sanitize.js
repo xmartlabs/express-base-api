d196d5e728a1c4493c33eaecee62f87b4fcd1513
Miguel Revetria <miguel@xmartlabs.com>
2018-01-16 13:42:12 -0300

    Added test suite

---
+++ tests/Sanitize.js
@@ -0,0 +1,8 @@
+const mocha = require('mocha');
+const expect = require('chai').expect;
+
+mocha.describe('Test environment', function() {
+  mocha.it('is OK', function() {
+    expect(true).to.equal(true);
+  });
+});
